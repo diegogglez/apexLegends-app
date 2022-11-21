721acaa31f7673747975c7742f5c31bacf475477
diegogglez <diegomiller7@gmail.com>
2022-11-21 15:40:54 +0100

    Feat: adding Legends-service

--- src/app/pages/legends/legends.component.ts
+++ src/app/pages/legends/legends.component.ts
@@ -1,3 +1,4 @@
+import { LegendsService } from './../../services/legends-service/legends.service';
 import { Component, OnInit } from '@angular/core';
 import PocketBase from 'pocketbase';
 
@@ -9,19 +10,21 @@ import PocketBase from 'pocketbase';
 export class LegendsComponent implements OnInit {
 
   myCharacters?: any[];
-  public pb = new PocketBase('http://127.0.0.1:8090');
 
-
-  constructor() { 
-
-    const getAllCharacters = async() => {
-      const result = await this.pb.collection('legends').getFullList();
-      console.log(result); 
-      this.myCharacters = [...result]    
-    }
-    getAllCharacters();
-
+  /* getLegends() {
+    this.legends.getAllLegends().then((result) => console.log(result))
+  } */
+  constructor(private legends: LegendsService) {
+    // this.getLegends()
+    this.legends.getAllLegends()
+      .then((result) => this.myCharacters = [...result])
+      .catch((error) => console.error(error));
   }
+
+
+  
+  
+
 
   ngOnInit(): void {
   }
